a0a32c2379cf55d925bd98955620b0e449fa2973
dav100ide <oreglio.dev@outlook.it>
2022-11-15 14:58:49 +0100

    stampa li con v-for

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue;
 createApp({
    data() {
       return {
-         message: 'Hello Vue!',
+         todoList: [
+            {
+               text: 'fare la spesa',
+               done: false,
+            },
+            {
+               text: 'allenarsi in palestra',
+               done: false,
+            },
+            {
+               text: 'uscire il cane',
+               done: false,
+            },
+         ],
       };
    },
    methods: {
