249dea972d7d46a12b98af896430426a72e67831
dav100ide <oreglio.dev@outlook.it>
2022-11-15 13:51:43 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+   data() {
+      return {
+         message: 'Hello Vue!',
+      };
+   },
+   methods: {
+      // metodi vue js
+   },
+}).mount('#app');
